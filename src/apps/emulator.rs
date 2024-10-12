22d62f219997f449d838a270d22cffccd0102b1b
uselessgoddess <dodickgod@gmail.com>
2024-10-12 18:43:38 +0300

    Add file picking to emulator

--- src/apps/emulator.rs
+++ src/apps/emulator.rs
@@ -1,17 +1,30 @@
 use {
   crate::{panels::MemoryEditor, widgets::HexEdit},
-  egui::{Color32, Context, RichText, Window},
+  egui::{
+    Button, Color32, Context, Key, KeyboardShortcut, Modifiers, RichText,
+    Window,
+  },
   egui_extras::{Size, StripBuilder},
+  egui_file_dialog::FileDialog,
+  std::fs,
 };
 
 #[derive(Default)]
 pub struct EmulatorPanel {
   xregs: Xregs,
   dram: Memory,
+
+  dialog: FileDialog,
 }
 
 impl EmulatorPanel {
   pub fn ui(&mut self, ctx: &Context) {
+    egui::TopBottomPanel::top("emulator-menu").show(ctx, |ui| {
+      egui::menu::bar(ui, |ui| {
+        self.file_menu_button(ui);
+      });
+    });
+
     self.dram.ui(ctx);
     Window::new("Registers")
       .collapsible(false)
@@ -19,9 +32,44 @@ impl EmulatorPanel {
       .show(ctx, |ui| {
         self.xregs.ui(ui);
       });
+
+    self.dialog.update(ctx);
+
+    if let Some(path) = self.dialog.take_selected() {
+      match fs::read(path) {
+        Ok(bytes) => self.dram.memory = bytes,
+        Err(err) => {}
+      }
+    }
   }
 
-  pub fn show_registers(&mut self, ui: &mut egui::Ui) {}
+  fn file_menu_button(&mut self, ui: &mut egui::Ui) {
+    let open_shortcut = KeyboardShortcut::new(Modifiers::CTRL, Key::O);
+
+    if ui.input_mut(|i| i.consume_shortcut(&open_shortcut)) {
+      self.dialog.select_file();
+    }
+
+    ui.menu_button("File", |ui| {
+      {
+        egui::gui_zoom::zoom_menu_buttons(ui);
+        ui.weak(format!(
+          "Current zoom: {:.0}%",
+          100.0 * ui.ctx().zoom_factor()
+        ))
+        .on_hover_text(
+          "The UI zoom level, on top of the operating system's default value",
+        );
+        ui.separator();
+      }
+
+      let button = Button::new("Open File")
+        .shortcut_text(ui.ctx().format_shortcut(&open_shortcut));
+      if ui.add(button).clicked() {
+        self.dialog.select_file();
+      }
+    });
+  }
 }
 
 pub struct Memory {
